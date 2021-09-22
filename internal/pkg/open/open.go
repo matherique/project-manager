83cab043cc7ef6bc6cd41196689ada858a843fdd
Matheus Henrique <matherique@gmail.com>
2021-09-21 21:12:34 -0300

    feat: add open subcommand

--- internal/pkg/open/open.go
+++ internal/pkg/open/open.go
@@ -2,6 +2,8 @@ package open
 
 import (
 	"log"
+	"os"
+	"os/exec"
 	"path"
 
 	"github.com/matherique/project-manager/pkg/config"
@@ -25,5 +27,23 @@ func (o *open) Exec(a []string) {
   o.c.Load()
   sp := o.c.Get("scripts")
   fp := path.Join(sp, a[0])
-  
+
+	p, err := exec.LookPath(fp)
+
+	if err != nil {
+    log.Fatalf("file not found: %v", err)
+	}
+
+	cmd := &exec.Cmd{
+		Path:   p,
+		Args:   []string{p, fp},
+		Stdout: os.Stdout,
+		Stdin:  os.Stdin,
+	}
+
+  err = cmd.Run()
+
+	if err != nil {
+    log.Fatalf("could not run the command: %v", err)
+	}
 }
