55d38967af6033aa0bfa5f0fd5ca71f9783b3099
Matheus Henrique <matherique@gmail.com>
2021-10-10 18:15:36 -0300

    feat: add `fzf` description into `help` subcommand

--- cmd/project/help.go
+++ cmd/project/help.go
@@ -19,6 +19,7 @@ Commands:
  config   List all configuration
  edit     Edit an project setup file
  remove   Remove and project
+ fzf      List all created projects using fzf and execute what you choose
 
 Use 'project [command] help' for more information about the command`
 	fmt.Fprintln(os.Stdout, doc)
