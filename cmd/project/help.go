b51d299ef77c09d9258087d644610c161714eda3
Matheus Henrique <matherique@gmail.com>
2021-10-17 18:04:54 -0300

    feat: Trim help doc in `help` command

--- cmd/project/help.go
+++ cmd/project/help.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	fc "github.com/matherique/project-manager/internal/file_config"
 )
@@ -15,13 +16,15 @@ Usage: project [command] [args]
 
 Commands:
  create   Create a new project
+ open     Start the project
  list     List all created projects
  config   List all configuration
  edit     Edit an project setup file
  remove   Remove and project
  fzf      List all created projects using fzf and execute what you choose
 
-Use 'project [command] help' for more information about the command`
-	fmt.Fprintln(os.Stdout, doc)
+Use 'project [command] help' for more information about the command
+`
+	fmt.Fprintln(os.Stdout, strings.Trim(doc, "\n"))
 	return nil
 }
