1d4af4e556f9c69df0f6598ec215230eda2c9677
Matheus Henrique <matherique@gmail.com>
2022-02-26 10:53:43 -0300

    refactor: apply changes on open command

--- cmd/project/open.go
+++ cmd/project/open.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	"github.com/matherique/project-manager/pkg/cmd"
-	fc "github.com/matherique/project-manager/pkg/config"
 	"github.com/matherique/project-manager/internal/project"
+	"github.com/matherique/project-manager/pkg/cmd"
+	"github.com/matherique/project-manager/pkg/config"
 )
 
 const doc_open string = `
@@ -14,20 +14,18 @@ Usage: project open [name]
 Execute the project script
 `
 
-func cmd_open(a []string, c fc.FileConfig) error {
-	if len(a) == 0 {
+func cmd_open(args []string, c config.Config, p project.Project) error {
+	if len(args) == 0 {
 		return fmt.Errorf("missing project name")
 	}
 
 	c.Load()
 
-	n := a[0]
+	name := args[0]
 
-	if !project.Exists(c, n) {
+	if !project.Exists(name) {
 		return fmt.Errorf("project not found")
 	}
 
-	fp := project.Path(c, n)
-
-	return cmd.Exec(fp, "up")
+	return cmd.Exec(p.Path(n), "up")
 }
